38cda47297b135b8b01268ad2f30e7f31b42fd9f
nova <nova.maene@gmail.com>
2020-09-21 07:15:15 +0200

    013

---
+++ 013RomanToInteger/index.js
@@ -0,0 +1,21 @@
+const romans = {
+	I:1,
+	V:5,
+	X:10,
+	L:50,
+	C:100,
+	D:500,
+	M:1000,
+};
+
+const romanToInt = function(s) {
+	let sum = 0;
+	const arrRoman = s.split('');
+
+	arrRoman.forEach((e, i) => {
+		romans[arrRoman[i + 1]] > romans[arrRoman[i]] ? sum -= romans[arrRoman[i]] : sum += romans[arrRoman[i]];
+	});
+	return(sum);
+};
+
+console.log(romanToInt('MCMXCIV'));
